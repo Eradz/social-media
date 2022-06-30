dab2a32c62491f9b50641f9bcc0c66d282f778c4
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2022-06-30 11:07:10 -0700

    new changes

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './login.css';
 import { Link } from 'react-router-dom';
 
 export const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [login, setLogin] = useState(false)
+  const [route, setRoute] = useState('')
+  
+ 
+  const onEmailChange = (e) => {
+    setEmail(e.target.value)
+  }
+  const onPasswordChange = (e) => {
+    setPassword(e.target.value)
+  }
+ 
+
+  const loging = () => {
+    
+    fetch('http://localhost:3000/signin', {
+      method: "post",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        email: email,
+        password: password
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data === 'success'){
+          setRoute('/home')
+        }
+      })
+      setLogin(!login)
+  }
+  
   return (
     
         <div className='login-container'>
@@ -21,11 +54,24 @@ export const Login = () => {
                       </Link>
                   </div>
                   <div className='login-inputs'>
-                    <input type="email" name="email" placeholder="Email Address"></input>
-                    <input type="password" name="password" placeholder='password'></input>
+                    <input 
+                    type="email" name="email" 
+                    placeholder="Email Address"
+                    value={email}
+                    onChange= {onEmailChange}
+                    ></input>
+                    <input type="password" name="password" 
+                    placeholder='password'
+                    value={password}
+                    onChange= {onPasswordChange}
+                    ></input>
                     <a href='#home'>Forgot password?</a>
-                  <Link className='link1' to='/home'>
-                  <button className='primary-btn'>Login</button>
+                  <Link className='link1' to={route}>
+                  <button  
+                  onClick={loging}
+                  className='primary-btn'>
+                  Login
+                  </button>
                   </Link>
                   <p>Not a member?  <Link to="/signup">Signup</Link></p>
                   </div>
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,8 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Signup.css'
 import { Link } from 'react-router-dom';
 
 export const Signup = () => {
+  const [route, setRoute] = useState("/signup")
+  const[firstname, setFirstname] = useState('')
+  const[lastname, setLastname] = useState('')
+  const[email,setEmail] = useState('')
+  const[password, setPassword] = useState('')
+
+const onFirstname = (e) => {
+  setFirstname(e.target.value)
+}
+const onLastname = (e) => {
+  setLastname(e.target.value)
+}
+const onEmail = (e) => {
+  setEmail(e.target.value)
+}
+const onPassword = (e) => {
+  setPassword(e.target.value)
+}
+
+
+  const [signup, setSignup]= useState(false)
+  
+
+  const loging = () => {
+    setSignup(!signup)
+  fetch("http://localhost:3000/signup",{
+    method: "post",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({
+      firstname: firstname,
+      lastname: lastname,
+      password: password,
+      email: email,
+    })
+  })
+  .then(response => response.json())
+  .then(users => {
+    if (users) {
+      console.log(users)
+      setRoute('/home')
+    }
+  })
+ 
+}
+
   return (
     <div className='signin-container'>
         <div className='signin-box'>
@@ -20,14 +65,42 @@ export const Signup = () => {
                     </Link>
                   </div>
             <div className='signin-inputs'>
-                  <input type="text" name="Firstname" placeholder='First name'></input>
-                  <input type="text" name="Lastname" placeholder='Last name'></input>
-                  <input type="email" name="email" placeholder="Email Address"></input>
-                  <input type="password" name="password" placeholder='password'></input>
-                  <a href='#home'>Forgot password?</a>
-                  <Link className='link2' to='/home'>
-                  <button className='primary-btn'>Signup</button>
+                  <input 
+                  type="text" name="firstname" 
+                  placeholder='firstname' 
+                  id='firstname'
+                  value={firstname}
+                  onChange={onFirstname}
+                  required/>
+                  <input
+                  type="text" name="lastname" 
+                  placeholder='lastname'
+                  id='lastname' 
+                  value={lastname}
+                  onChange={onLastname}
+                  required/>
+                  <input
+                  type="email" name="email" 
+                  placeholder="email" 
+                  id='email'
+                  value={email}
+                  onChange={onEmail}
+                  required/>
+                  <input
+                  type="password" name="password" 
+                  placeholder='password'
+                  id='password' 
+                  value={password}
+                  onChange={onPassword}
+                  required/>
+                  <Link className='link2' to={route}>
+                  <button 
+                  onClick={loging}
+                  type='submit' className='primary-btn'>
+                  Signup
+                  </button>
                   </Link>
+                  <a href='#home'>Forgot password?</a>
                 <p>Already a member? <Link to="/">Login</Link></p>
             </div>
 
